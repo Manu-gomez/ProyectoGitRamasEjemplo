443ac5a59803c30c57b7bcc13ff5e76bddabe764
Manu-gomez <75076957+Manu-gomez@users.noreply.github.com>
2020-12-03 10:54:43 +0100

    Update ProyectoGitRamasEjemplo.java
    Funcion imprime saludo

--- src/ProyectoGitRamasEjemplo.java
+++ src/ProyectoGitRamasEjemplo.java
@@ -4,5 +4,7 @@ public class ProyectoGitRamasEjemplo {
 	public static void main(String[] args) {
 System.out.println("Hola mundo");
 	}
-
+	public static void imprimeSaludo() { 
+		System.out.println(“Hei”);	
+	}
 }
